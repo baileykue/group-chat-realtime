54a197faaecccd8212adaeff8785ffa30cf677d8
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-07-14 11:38:08 -0700

    moved header and footer, changed where variables stored, ignore client file

--- services/client.js
+++ services/client.js
@@ -1,4 +1,5 @@
-export const client = supabase.createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const SUPABASE_URL = 'https://imxwlxgfzzlmfosvefsz.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlteHdseGdmenpsbWZvc3ZlZnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc4MTc1NzgsImV4cCI6MTk3MzM5MzU3OH0.K3EQSXdVfuCZDo4FKmEZc6vU9VlVk17KSHml4jodW5I';
+
+export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
